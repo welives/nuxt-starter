7c08573acbcb543ac987dbfb2b054f2b03670d00
Jandan <lutao1096@gmail.com>
2023-10-14 22:06:26 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+delete colors.lightBlue
+delete colors.warmGray
+delete colors.trueGray
+delete colors.coolGray
+delete colors.blueGray
+export default {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
+  ],
+  theme: {
+    colors: {
+      ...colors,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
